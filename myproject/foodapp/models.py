449b5acb5821072851efe90552d9852e27f05d2b
prosenjeetshil <prosenjeetshil@gmail.com>
2023-02-22 15:53:29 +0530

    refactor: model field updated

--- myproject/foodapp/models.py
+++ myproject/foodapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Food(models.Model):
-    fid = models.IntegerField()
     fname = models.CharField(max_length=30)
-    fdesc = models.CharField(max_length=150)
+    fdesc = models.CharField(max_length=300)
     fprice = models.IntegerField()
+    fimg = models.CharField(max_length=300)
