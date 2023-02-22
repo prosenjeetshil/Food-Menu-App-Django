eaba38308d6d72328705df433e1cd70dcf9a98f2
prosenjeetshil <prosenjeetshil@gmail.com>
2023-02-22 12:10:45 +0530

    chore: model and migration

--- myproject/foodapp/models.py
+++ myproject/foodapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Food(models.Model):
+    fid = models.IntegerField()
+    fname = models.CharField(max_length=30)
+    fdesc = models.CharField(max_length=150)
+    fprice = models.IntegerField()
