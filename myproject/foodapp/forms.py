ddcf2d97d479faa2bfab74370d61974c526efbb6
prosenjeetshil <prosenjeetshil@gmail.com>
2023-02-22 15:53:53 +0530

    refactor: form fields updated

--- myproject/foodapp/forms.py
+++ myproject/foodapp/forms.py
@@ -1,4 +1,4 @@
-from models import Food
+from .models import Food
 from django import forms
 
 class FoodForm(forms.ModelForm):
@@ -7,15 +7,15 @@ class FoodForm(forms.ModelForm):
         fields = '__all__'
 
         labels = {
-            'fid': 'Food ID',
             'fname': 'Food Name',
             'fdesc': 'Food Description',
-            'fprice': 'Food Price'
+            'fprice': 'Food Price',
+            'fimg': 'Food image URL'
         }
 
         widgets = {
-            'fid': forms.NumberInput(attrs={'placholder': 'e.g 100'}),
             'fname': forms.TextInput(attrs={'placeholder': 'e.g. Butter Chicken'}),
-            'fdesc': forms.TextInput(attrs={'placholder': 'e.g Butter chicken, traditionally known as murgh makhani, is an Indian dish originating in New Delhi.'}),
-            'fprice': forms.NumberInput(attrs={'placholder': 'e.g ₹449'})
+            'fdesc': forms.Textarea(attrs={'placholder': 'e.g Butter chicken, traditionally known as murgh makhani, is an Indian dish originating in New Delhi.'}),
+            'fprice': forms.NumberInput(attrs={'placholder': 'e.g ₹449'}),
+            'fimg': forms.TextInput(attrs={'placeholder': 'Enter URL'})
         }
