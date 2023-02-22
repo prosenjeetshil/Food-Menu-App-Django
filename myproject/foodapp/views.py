c5cc633e4569f826ef84f6f2a36cfb8e09d37aae
prosenjeetshil <prosenjeetshil@gmail.com>
2023-02-22 15:55:10 +0530

    chore: create view

--- myproject/foodapp/views.py
+++ myproject/foodapp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+
+from .forms import FoodForm
 
 # Create your views here.
+def addFoodView(request):
+    form = FoodForm()
+    if request.method == 'POST':
+        form = FoodForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Data saved!')
+    template_name = 'foodapp/addDetails.html'
+    context = {'form': form}
+    return render(request, template_name, context)
+
