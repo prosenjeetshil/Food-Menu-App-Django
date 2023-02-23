5d9c88e5a52cfed0fa47c8128a82c4e7cfc38531
prosenjeetshil <prosenjeetshil@gmail.com>
2023-02-23 11:50:55 +0530

    feat: crud operations

--- myproject/foodapp/urls.py
+++ myproject/foodapp/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('add/', views.addFoodView, name='add'),
     path('menu/',views.showDetailsView, name='show'),
+    path('edit/', views.editDetailsView, name='edit'),
+    path('up/<int:id>',views.updateDetailsView, name='update'),
+    path('del/<int:id>', views.deleteDetailsView, name='delete'),
 ]
--- myproject/foodapp/views.py
+++ myproject/foodapp/views.py
@@ -9,7 +9,7 @@ def addFoodView(request):
         form = FoodForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponse('Data saved!')
+            return redirect('show')
     template_name = 'foodapp/addDetails.html'
     context = {'form': form}
     return render(request, template_name, context)
@@ -17,5 +17,32 @@ def addFoodView(request):
 def showDetailsView(request):
     obj = Food.objects.all()
     template_name = 'foodapp/showDetails.html'
+    context = {'obj': obj}
+    return render(request, template_name, context)
+
+def editDetailsView(request):
+    obj = Food.objects.all()
+    template_name = 'foodapp/editDetails.html'
+    context = {'obj': obj}
+    return render(request, template_name, context)
+
+def updateDetailsView(request,id):
+    obj = Food.objects.get(id=id)
+    form = FoodForm(instance=obj)
+    if request.method == 'POST':
+        form = FoodForm(request.POST,instance=obj)
+        if form.is_valid():
+            form.save()
+            return redirect('show')
+    template_name = 'foodapp/addDetails.html'
+    context = {'form': form}
+    return render(request, template_name, context)
+
+def deleteDetailsView(request,id):
+    obj = Food.objects.get(id=id)
+    if request.method == 'POST':
+        obj.delete()
+        return redirect('show')
+    template_name = 'foodapp/confirmDelete.html'
     context = {'obj': obj}
     return render(request, template_name, context)
